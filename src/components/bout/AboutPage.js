7bfe80dfbf1dfd03bfc6461b5e3f04f51972d08e
David Florez <dFlorezr@psl.com.co>
2017-02-07 14:29:29 -0500

    routes, index and about pages

---
+++ src/components/bout/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render() {
+        return (
+            <div className="jumbotron">
+                <h1>About</h1>
+                <p>This App use react, redux and react-router</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
