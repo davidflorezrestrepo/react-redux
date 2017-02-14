7d00f17a879f5dd2d5bdf890ebb7c59c869e1b94
David Florez <dFlorezr@psl.com.co>
2017-02-14 15:54:34 -0500

    add thunk for async calls

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import './styles/styles.css';
+import {loadCourses} from './actions/courseActions'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
     <Provider store={store}>
         <Router history={browserHistory} routes={routes}/>
-    </Provider>, 
+    </Provider>,
     document.getElementById('app')
 );
