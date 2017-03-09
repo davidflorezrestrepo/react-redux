79daf9263e346bb97b14701af7c0cc5b5c2936e6
David Florez <dFlorezr@psl.com.co>
2017-03-09 16:26:50 -0500

    production configuration setup

---
+++ tools/build.js
@@ -0,0 +1,32 @@
+/*eslint-disable no-console */
+import webpack from 'webpack';
+import webpackConfig from '../webpack.config.prod';
+import colors from 'colors';
+
+process.env.NODE_ENV = 'production';
+
+console.log('Generating minified bundle for prouction via webpack. This will take a moment...'.blue);
+
+webpack(webpackConfig).run((err, stats) => {
+    if (err) {
+        console.log(err.bold.red);
+        return 1;
+    }
+
+    const jsonStats = stats.toJson();
+
+    if (jsonStats.hasErrors) {
+        return jsonStats.errors.map(error => console.log(error.red));
+    }
+
+    if (jsonStats.hasWarnings) {
+        console.log('webpack generate the following warnings: '.bold.yellow);
+        jsonStats.warnings.map(warning => console.log(warning.yellow));
+    }
+
+    console.log(`webpack stats: ${stats}`);
+
+    console.log('Your App has been compiled in production mode and written to /dist. It\'s ready to roll'.green);
+
+    return 0;
+});
